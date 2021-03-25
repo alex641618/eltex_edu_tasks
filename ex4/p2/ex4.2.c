4462209eb3d075bd4fe2e846258e711c3d03cba4
alex641618 <aleksei-alushkin@mail.ru>
2021-03-25 10:24:11 +0700

    изменен тип массива при присвоении адресу структуры

--- ex4/p2/ex4.2.c
+++ ex4/p2/ex4.2.c
@@ -8,7 +8,7 @@ int main() {
 
 	struct test *my_test;
 	char str[10]={'A', 1, 0, 0, 0, 'B', 0, 0, 75, 0};
-	my_test=str;
+	my_test=(struct test*) str;
 	printf ("my_test.a=%c\nmy_test.b=%d\n", my_test->a, my_test->b);
 	printf ("(my_test+1).a=%c\n(my_test+1).b=%d\n", (my_test+1)->a, (my_test+1)->b );
 	return 0;
