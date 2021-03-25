1e3bbaa44980c23372103a3e5ecc7a79a9a6de68
alex641618 <aleksei-alushkin@mail.ru>
2021-03-25 10:14:31 +0700

    программа 3 не завершена

---
+++ ex4/p2/ex4.2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	struct test {
+		char a;
+		int b;
+	};
+
+	struct test *my_test;
+	char str[10]={'A', 1, 0, 0, 0, 'B', 0, 0, 75, 0};
+	my_test=str;
+	printf ("my_test.a=%c\nmy_test.b=%d\n", my_test->a, my_test->b);
+	printf ("(my_test+1).a=%c\n(my_test+1).b=%d\n", (my_test+1)->a, (my_test+1)->b );
+	return 0;
+}
+
+
