3d3366c934a805d42c2380ed29ac0410b0a4a583
alex641618 <aleksei-alushkin@mail.ru>
2021-05-11 22:07:44 +0700

    калькулятор(динамическая библиотека)

---
+++ ex8/diff.c
@@ -0,0 +1,6 @@
+int diff(int param1, int param2)
+{
+	int result;
+	result = param1-param2;
+	return result;
+}
---
+++ ex8/my_kalc.h
@@ -0,0 +1,4 @@
+int summ(int,int);
+int diff(int,int);
+int pro(int,int);
+int rat(int,int);
